af3dc24b49ce64b740296a85cd915ea733a58a1e
jiahua-zhoujian <zhoujian@rockontrol.com>
2020-06-30 17:44:50 +0800

    how to use flag

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+// flag包的运行方式:(remember:在运行时记得执行flag.Parse()方法!!!)
+// 1.go run main.go -greet=goodbye -debug=true
+// 2.go build后,./main -debug=true -greet=goodbye
+// 3.编辑goland的configuration中心,添加对应的启动参数
+var (
+	filePath = flag.Bool("debug", false, "")
+	str      = flag.String("greet", "hello", "")
+)
 
 func main() {
-
-	for i := 0; i < 10; i++ {
-		fmt.Println(i%2 == 0)
-	}
+	flag.Parse()
+	fmt.Println(*filePath)
+	fmt.Println(*str)
 }
