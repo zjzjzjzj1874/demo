76f5e4fd5099376ba4c959e31a8675d70bac78ea
jiahua-zhoujian <zhoujian@rockontrol.com>
2020-06-30 11:06:51 +0800

    golang channel for range use

--- task/task.go
+++ task/task.go
@@ -3,14 +3,14 @@ package task
 type TaskChan struct {
 	Desc       string
 	Input      int
-	Task       chan interface{}
+	Task       chan int
 	Output     string
 	ExitSignal chan int
 }
 
 func NewTaskChan(desc string) *TaskChan {
 	tc := &TaskChan{Desc: desc}
-	tc.Task = make(chan interface{}, 100)
+	tc.Task = make(chan int, 100)
 	tc.ExitSignal = make(chan int, 1)
 	return tc
 }
--- task/task_test.go
+++ task/task_test.go
@@ -36,3 +36,29 @@ func TestNewTaskChan(t *testing.T) {
 
 	time.Sleep(time.Hour)
 }
+
+// for range读取channel
+func TestNewTaskChan1(t *testing.T) {
+	retry := func(i int) bool {
+		ok := i%2 == 0
+		return ok
+	}
+	go func() {
+		// 场景：当需要不断从channel读取数据时
+		// 原理：使用for-range读取channel，这样既安全又便利，当channel关闭时，for循环会自动退出，
+		// 		无需主动监测channel是否关闭，可以防止读取已经关闭的channel，造成读到数据为通道所存储的数据类型的零值。
+		for x := range taskChan.Task {
+			fmt.Println(x)
+			if retry(x) { // 添加过滤条件 ==> 满足条件可以忽略,不满足可以继续retry
+				taskChan.Task <- x + 1
+			}
+		}
+	}()
+
+	for i := 0; i < 10; i++ {
+		taskChan.Task <- i
+		time.Sleep(time.Second * 3)
+	}
+
+	time.Sleep(time.Hour)
+}
