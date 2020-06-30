58bbc9f49464de18a0a88d80cd54fa4d23d2112b
jiahua-zhoujian <zhoujian@rockontrol.com>
2020-06-30 18:12:29 +0800

    flag more aciton

--- main.go
+++ main.go
@@ -9,13 +9,18 @@ import (
 // 1.go run main.go -greet=goodbye -debug=true
 // 2.go build后,./main -debug=true -greet=goodbye
 // 3.编辑goland的configuration中心,添加对应的启动参数
-var (
-	filePath = flag.Bool("debug", false, "")
-	str      = flag.String("greet", "hello", "")
-)
-
+// go run main.go -h ==> 可以查看哪些启动参数可以配置
+// 更多玩法,敬请参考:https://books.studygolang.com/The-Golang-Standard-Library-by-Example/chapter13/13.1.html
 func main() {
+	d := new(bool)
+	flag.BoolVar(d, "debug", false, "Whether debug mode is enabled")
+	v := flag.Bool("v", false, "show version")
 	flag.Parse()
-	fmt.Println(*filePath)
-	fmt.Println(*str)
+	fmt.Println(*d)
+	if *v {
+		fmt.Println("v1.0.0")
+	}
+	if *d {
+		fmt.Println(*d)
+	}
 }
