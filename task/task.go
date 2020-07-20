ecba8a8db638df65a0f4d94aeb52898e915907f6
jiahua-zhoujian <zhoujian@rockontrol.com>
2020-07-20 11:14:15 +0800

    chore:how to use deprecated

--- task/task.go
+++ task/task.go
@@ -14,3 +14,11 @@ func NewTaskChan(desc string) *TaskChan {
 	tc.ExitSignal = make(chan int, 1)
 	return tc
 }
+
+// deprecated
+func NewTaskChannel(desc string) *TaskChan {
+	tc := &TaskChan{Desc: desc}
+	tc.Task = make(chan int, 100)
+	tc.ExitSignal = make(chan int, 1)
+	return tc
+}
