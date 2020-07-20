b80d6eae882b9122a0670f1432572c68a7283053
jiahua-zhoujian <zhoujian@rockontrol.com>
2020-07-20 11:15:12 +0800

    feat:plain uml:how to draw a uml

---
+++ go.mod
@@ -0,0 +1,10 @@
+module demo
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/mojocn/base64Captcha v1.3.1
+	github.com/willf/bitset v1.1.10
+	google.golang.org/protobuf v1.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"demo/task"
 	"fmt"
-	"math"
+	"time"
 )
 
 func main() {
-	flagUse()
+
+	//flagUse()
+
 	//go func() {
-	//	for i := 0; i < 10; i++ {
+	//	for i := 0; i < 10; i {
 	//		fmt.Println(i)
 	//	}
 	//}()
@@ -27,6 +30,90 @@ func main() {
 	//fmt.Println(strings.Contains(src, sub))
 	//fmt.Println(strings.Count(src, sub))
 
-	fmt.Println(math.Pow(2.0, 3.0))
+	//go task()
+	//time.Sleep(time.Hour)
 
+	m := make(map[string]CPUInfo)
+	m["1"] = CPUInfo{
+		UsedPercent: 50,
+		Framework:   "arm",
+		Model:       "i7",
+	}
+	m["2"] = CPUInfo{
+		UsedPercent: 60,
+		Framework:   "arm",
+		Model:       "i5",
+	}
+
+	for k, v := range m {
+		v.UsedPercent += 10
+		m[k] = v
+	}
+	time.Sleep(time.Second)
+	fmt.Println(m)
 }
+
+func taskTimer() {
+
+	// 弃用标识符用法
+	task.NewTaskChannel("init task")
+
+	ticker := time.NewTicker(3 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("time = ", time.Now())
+		default:
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+type NodeType int8
+
+const (
+	NodeTypeNull     NodeType = iota
+	NodeTypeEdge              // 边缘设备
+	NodeTypeGPUServe          // GPU服务器
+)
+
+// 边缘设备/GPU服务器节点信息
+type (
+	NodeInfo struct {
+		NodeID        int          // 节点ID
+		NodeType      NodeType     // 节点类型
+		UpgradeRecord string       // 升级记录
+		Os            string       // 操作系统
+		Location      string       // 地理位置
+		RegisterTime  string       // 注册时间
+		Lists         []ServerInfo // 服务器列表信息
+	}
+
+	ServerInfo struct {
+		CPUInfo           // cpu信息
+		MemInfo           // 内存信息
+		DiskInfo          // 磁盘信息
+		LoadInfo int      // 负载情况
+		IP       string   // IP地址
+		Name     string   // 服务器名字
+		Models   []string // 设备/服务器上跑的模型
+	}
+
+	DiskInfo struct {
+		UsedPercent int // 利用率
+		TotalMem    int // 总内存(GB)
+		UsedMem     int // 已使用(GB)
+	}
+
+	MemInfo struct {
+		UsedPercent int // 利用率
+		TotalMem    int // 总内存(TB)
+		UsedMem     int // 已使用(TB)
+	}
+
+	CPUInfo struct {
+		UsedPercent int    // 利用率
+		Framework   string // 架构
+		Model       string // 型号
+	}
+)
